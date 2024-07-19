6835f4c1e8d1e5a885dfe8e26c503a095caf97be
AriRainbow <mariel.williams@atlasschool.com>
2024-07-19 04:34:40 +0000

    Setting up files

---
+++ TEST_main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the shell program.
+ *
+ * Description: This function initializes the shell, displays a
+ * prompt, reads user input, parses the input into commands, and
+ * executes the commands.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	....
+}
