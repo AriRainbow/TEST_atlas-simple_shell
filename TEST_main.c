f71fd4299919331c53584ff3fa8672b4eb64eaa2
AriRainbow <mariel.williams@atlasschool.com>
2024-07-19 07:44:49 +0000

    Starter TEST_main.c and TEST_shell.h

--- TEST_main.c
+++ TEST_main.c
@@ -11,5 +11,26 @@
  */
 int main(void)
 {
-	....
+	char *line = NULL; /* pointer stores input line */
+	size_t len = 0; /* size of input buffer */
+	ssize_t nread; /* number of characters read by getline */
+	char **args; /* array of arguments parsed from input line */
+	int status = 1; /* status of shell, 1 running 0 exit */
+
+	while (status) /* loop until status is 0, exit command */
+	{
+		printf("($) "); /* display the prompt */
+		nread = getline(&line, &len, stdin); /* read input line */
+		if (nread == -1) /* check for error reading input */
+		{
+			free(line); /* free alloc mem for line */
+			perror("getline"); /* print error message */
+			exit(EXIT_FAILURE); /* exit with failure status */
+		}
+		args = parse_line(line); /* parse input into arguments */
+		status = execute(args); /* execute parsed commands */
+		free(args); /* free alloc mem for args */
+	}
+	free(line); /* free alloc mem for line before exiting */
+	return (0); /* success status */
 }
--- TEST_shell.h
+++ TEST_shell.h
@@ -1,2 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
+
+#include <stdio.h> /* printf, perror, getline */
+#include <stdlib.h> /* free, exit, EXIT_FAILURE */
+#include <unistd.h> /* POSIX API functions */
+#include <string.h> /* string manipualtion */
+
+char **parse_line(char *line);
+int execute(char **args);
+
+#endif
